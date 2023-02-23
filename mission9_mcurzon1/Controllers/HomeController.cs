b587322fbbc720b6226e2348e09849ad278e3780
mattcurzon <mcurzon1@byu.edu>
2023-02-23 00:08:21 -0700

    Add project files.

---
+++ mission9_mcurzon1/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using mission9_mcurzon1.Models;
+using mission9_mcurzon1.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mission9_mcurzon1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private BookstoreContext context { get; set; }
+        private IBookstoreRepository repo;
+
+        public HomeController(ILogger<HomeController> logger, IBookstoreRepository temp)
+        {
+            _logger = logger;
+            repo = temp;
+        }
+
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 10;
+
+            var x = new BooksViewModel
+            {
+                Books = repo.Books
+                .OrderBy(x => x.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+            return View(x);
+        }
+
+        public IActionResult Privacy()
+        {
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult AllBooks()
+        {
+            var books = repo.Books.ToList();
+            return View(books);
+        }
+    }
+}
---
+++ mission9_mcurzon1/Models/ViewModels/PageInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mission9_mcurzon1.Models.ViewModels
+{
+    public class PageInfo
+    {
+        public int TotalNumBooks { get; set; }
+        public int BooksPerPage { get; set; }
+        public int CurrentPage { get; set; }
+        //calculate number of pages needed
+        public int TotalPages => (int) Math.Ceiling((double) TotalNumBooks / BooksPerPage);
+    }
+}
